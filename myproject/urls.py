f03507c2d92b586506528becbb1fb8590cee1ea4
Rosalyn Hatcher <r.s.hatcher@reading.ac.uk>
2019-08-01 16:18:44 +0100

    Tutorial status - End of Part 5

--- myproject/urls.py
+++ myproject/urls.py
@@ -49,5 +49,7 @@ urlpatterns = [
          name='password_change_done'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     path('admin/', admin.site.urls),
 ]
